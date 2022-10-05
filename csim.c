5e8eccb73ffa541e206eb7e980b6b955f5e2245a
Oscarwasoccupied <z_xw99@outlook.com>
2022-10-05 11:03:41 -0400

    create csim.c file

---
+++ csim.c
@@ -0,0 +1,12 @@
+/**************************************************
+ * @ File Name: csim.c
+ * @ Author: Xianwei Zou
+ * @ Version: 0.0.0
+ * @ Description: Simulate the behavior of a cache
+ **************************************************/
+
+#include "cachelab.h"
+
+int main(int argc, char *argv[]) {
+    
+}
